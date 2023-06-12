213e19d9a18f25a8dc9863c903e2b36e053679c7
XuDong Wu <118393772+OrangeSpace0721@users.noreply.github.com>
2023-06-12 15:37:46 +0100

    Update Qconfig.py

--- Qconfig.py
+++ Qconfig.py
@@ -6,7 +6,7 @@ Created on Tue May  3 23:18:47 2022
 """
 
 
-APIToken = """f18d015e45c92c2d20716b2bd643f42d412cc5c4ec0a2ce700c119505dcc1c637635242615e0664034c17b111d984d08fa52d7707a9e619f6fc70d09fbc4e73f"""
+APIToken = """your token here"""
 
 config = {
     'url': "https://quantumexperience.ng.bluemix.net/api"
